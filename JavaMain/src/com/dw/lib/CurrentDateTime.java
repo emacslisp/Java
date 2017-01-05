0d7d49c3bf30cf857c79022b1764823094faf4c6
wudilab <wudilinux@gmail.com>
2017-01-05 12:45:41 +1100

    add new java project

---
+++ JavaMain/src/com/dw/lib/CurrentDateTime.java
@@ -0,0 +1,17 @@
+package com.dw.lib;
+
+import java.time.*;
+
+public class CurrentDateTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate dNow = LocalDate.now();
+		System.out.println(dNow);
+		LocalTime tNow = LocalTime.now();
+		System.out.println(tNow);
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
+	}
+
+}
