6a314174d5c3c6ea955d38c71b6f312293c5ecbb
wudilab <wudilinux@gmail.com>
2017-01-05 13:39:35 +1100

    test

--- JavaMain/src/com/dw/lib/CurrentDateTime.java
+++ JavaMain/src/com/dw/lib/CurrentDateTime.java
@@ -4,6 +4,7 @@ import java.time.*;
 
 public class CurrentDateTime {
 
+	//@example: 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LocalDate dNow = LocalDate.now();
