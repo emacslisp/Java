7196f1ad18bec12ee37d46249cb0e0829a55e145
emacslisp <wudi850620@hotmail.com>
2017-02-05 23:19:14 +1100

    java swing project for menu

---
+++ JavaMain/src/com/dw/swing/menuTest.java
@@ -0,0 +1,10 @@
+package com.dw.swing;
+
+public class menuTest {
+//@todo: Java Project for Swing as file explore
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
