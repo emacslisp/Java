91db3ae06f401bf322af58a11a89fe9430fa1d36
emacslisp <wudi850620@hotmail.com>
2017-03-08 15:08:29 +1100

    url to string

---
+++ JavaMain/src/com/dw/lib/UrlToString.java
@@ -0,0 +1,47 @@
+package com.dw.lib;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UrlToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String result = getUrlAsString("http://www.baidu.com");
+		System.out.println(result);
+	}
+	
+	public static String getUrlAsString(String url)
+	{
+	    try
+	    {
+	        URL urlObj = new URL(url);
+	        URLConnection con = urlObj.openConnection();
+
+	        con.setDoOutput(true); // we want the response 
+	        con.setRequestProperty("Cookie", "myCookie=test123");
+	        con.connect();
+
+	        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+	        StringBuilder response = new StringBuilder();
+	        String inputLine;
+
+	        String newLine = System.getProperty("line.separator");
+	        while ((inputLine = in.readLine()) != null)
+	        {
+	            response.append(inputLine + newLine);
+	        }
+
+	        in.close();
+
+	        return response.toString();
+	    }
+	    catch (Exception e)
+	    {
+	        throw new RuntimeException(e);
+	    }
+	}
+
+}
