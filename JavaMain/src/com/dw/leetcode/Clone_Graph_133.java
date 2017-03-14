5f135fe2280242eb875c0f53be3576168c694397
emacslisp <wudi850620@hotmail.com>
2017-03-14 16:40:19 +1100

    add 133 clone graph

---
+++ JavaMain/src/com/dw/leetcode/Clone_Graph_133.java
@@ -0,0 +1,38 @@
+package com.dw.leetcode;
+
+import java.util.*;
+
+//@todo: finish clone graph with 133
+
+public class Clone_Graph_133 {
+	static class UndirectedGraphNode {
+		int label;
+		List<UndirectedGraphNode> neighbors;
+
+		UndirectedGraphNode(int x) {
+			label = x;
+			neighbors = new ArrayList<UndirectedGraphNode>();
+		}
+	};
+	
+    public static UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    	if(node == null) return null;
+    	
+    	Map<Integer,UndirectedGraphNode> nodeMap = new HashMap<Integer,UndirectedGraphNode>();
+    	Map<Integer,UndirectedGraphNode> rootMap = new HashMap<Integer,UndirectedGraphNode>();
+    	
+    	UndirectedGraphNode root = new UndirectedGraphNode(node.label);
+    	
+    	
+    	
+    	return root;
+    }
+    
+    public static void foo() {
+    	
+    }
+
+	public static void main(String[] args) {
+
+	}
+}
