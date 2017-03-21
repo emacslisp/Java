653d9309a2c37ecdf5c80a2443cb3a5de468bdaf
emacslisp <wudi850620@hotmail.com>
2017-03-21 14:22:16 +1100

    Basic Authentication

---
+++ JavaMain/src/com/dw/lib/BasicAuthentication.java
@@ -0,0 +1,22 @@
+package com.dw.lib;
+
+import java.util.Base64;
+
+public class BasicAuthentication {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name = "admin";
+		String password = "admin";
+
+		String authString = name + ":" + password;
+		System.out.println("auth string: " + authString);
+		byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
+		String authStringEnc = new String(authEncBytes);
+		System.out.println("Base64 encoded auth string: " + authStringEnc);
+	}
+
+}
