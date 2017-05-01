74a4ca69716d3382921cc739195df8878b7c8b3f
emacslisp <wudi850620@hotmail.com>
2017-05-01 14:55:17 +1000

    add  java method parser

---
+++ JavaMain/src/com/dw/parser/MethodParser.java
@@ -0,0 +1,15 @@
+package com.dw.parser;
+
+public class MethodParser {
+	
+	public MethodParser() {
+		Thread.dumpStack();
+	}
+	
+	public static void main(String[] args) {
+		MethodParser m = new MethodParser();
+		
+		System.out.println(m);
+	}
+	
+}
