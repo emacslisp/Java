3fee2a2a1c5eeccbc9a93d686cff8ac8030e0dd4
emacslisp <wudi850620@hotmail.com>
2017-05-16 10:26:24 +1000

    update leetcode 28 and 486

--- JavaMain/src/com/dw/leetcode/Predict_the_Winner_486.java
+++ JavaMain/src/com/dw/leetcode/Predict_the_Winner_486.java
@@ -2,91 +2,21 @@ package com.dw.leetcode;
 
 public class Predict_the_Winner_486 {
 	
-	/*
-	 * int[] choosed = new int[nums.size];
-	 * int begin = 0;
-	 * int end = nums.size-1;
-	 * int counter = 0;
-	 * int play1 = 0;
-	 * int play2 = 0;
-	 * bool finalResult = false;
-	 * foo(play1,play2,begin,end,counter) {
-	 *    
-	 * 	  if(begin > end) { 
-	 * 		 if(play1 > play2) finalResult = true;
-	 *       return;
-	 *    }
-	 *    
-	 *    //play 1 choose 1
-	 *    choosed[begin] = 1;
-	 *    int tempPlay1 = play1;
-	 *    int tempPlay2 = play2;
-	 *    if(counter %2 == 0)
-	 *    	tempPlay1 = play1 + nums[begin];
-	 *    
-	 *    if(counter %2 == 1)
-	 *    	tempPlay2 = play2 + nums[begin];
-	 * 	  
-	 *    foo(tempPlay1,tempPlay2, begin + 1,end, counter+1);
-	 *    
-	 *    tempPlay1 = play1;
-	 *    tempPlay2 = play2;
-	 *     //play 1 choose 2
-	 *    choosed[end] = 1;
-	 *    if(counter %2 == 0)
-	 *    	tempPlay1 = play1 + nums[end];
-	 *    
-	 *    if(counter %2 == 1)
-	 *    	tempPlay2 = play2 + nums[end];
-	 *    foo(tempPlay1,tempPlay2,begin,end - 1, counter+1);
-	 *    
-	 * 
-	 * }
-	 */
-	//@example: recursive bruce force.
-	public static boolean foo(int play1,int play2, int begin,int end,int counter,int[] nums,int[] choosed) {
-		if(begin > end) { 
-		    if(play1 > play2) return true;
-		    return false;
-		}
 
-		//play 1 choose 1
-		choosed[begin] = 1;
-		int tempPlay1 = play1;
-		int tempPlay2 = play2;
-		if(counter %2 == 0)
-		    tempPlay1 = play1 + nums[begin];
-
-		if(counter %2 == 1)
-		    tempPlay2 = play2 + nums[begin];
-		    
-		boolean result = foo(tempPlay1,tempPlay2, begin + 1,end, counter+1,nums,choosed);
-		
-		if(result) return true;
-
-		tempPlay1 = play1;
-		tempPlay2 = play2;
-		//play 1 choose 2
-		choosed[end] = 1;
-		if(counter %2 == 0)
-		    tempPlay1 = play1 + nums[end];
-
-		if(counter %2 == 1)
-		    tempPlay2 = play2 + nums[end];
-		result = foo(tempPlay1,tempPlay2,begin,end - 1, counter+1,nums,choosed);
-		
-		if(result) return true;
-		
-		return false;
-	}
-	
 	public static boolean PredictTheWinner(int[] nums) {
-		int[] choosed = new int[nums.length];
-		
-		return foo(0,0,0,nums.length-1,0,nums,choosed);
+		    int n = nums.length;
+		    int[][] dp = new int[n][n];
+		    for (int i = 0; i < n; i++) { dp[i][i] = nums[i]; }
+		    for (int len = 1; len < n; len++) {
+		        for (int i = 0; i < n - len; i++) {
+		            int j = i + len;
+		            dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+		        }
+		    }
+		    return dp[0][n - 1] >= 0;
     }
 	public static void main(String[] args) {
-		int[] test = {1, 5, 2};
+		int[] test = {1, 5, 233, 7};
 		
 		System.out.println(PredictTheWinner(test));
 	}
