eb679d9b75e80dbd3ddcd346524af783cedd4a2e
emacslisp <java.gdb@gmail.com>
2017-06-18 01:06:07 +1000

    add windows close listener

--- JavaMain/src/com/dw/notepad2/Notepad2.java
+++ JavaMain/src/com/dw/notepad2/Notepad2.java
@@ -2,6 +2,8 @@ package com.dw.notepad2;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.*;
 import java.util.*;
 
@@ -16,6 +18,12 @@ public class Notepad2 extends JPanel {
 	private static ResourceBundle resources;
 	private final static String EXIT_AFTER_PAINT = "-exit";
 	private static boolean exitAfterFirstPaint;
+	
+	private static final String[] MENUBAR_KEYS = { "file", "edit", "debug" };
+	private static final String[] TOOLBAR_KEYS = { "new", "open", "save", "-", "cut", "copy", "paste" };
+	private static final String[] FILE_KEYS = { "new", "open", "save", "-", "exit" };
+	private static final String[] EDIT_KEYS = { "cut", "copy", "paste", "-", "undo", "redo" };
+	private static final String[] DEBUG_KEYS = { "dump", "showElementTree" };
 
 	static {
 		try {
@@ -25,6 +33,16 @@ public class Notepad2 extends JPanel {
 		} catch (MissingResourceException | IOException e) {
 			System.err.println("resources/Notepad.properties " + "or resources/NotepadSystem.properties not found");
 			System.exit(1);
+		}
+	}
+	
+	protected static final class AppCloser extends WindowAdapter {
+
+		@Override
+		public void windowClosing(WindowEvent e)
+		{
+			System.out.println("AppCloser!!!");
+			System.exit(0);
 		}
 	}
 	
@@ -44,6 +62,7 @@ public class Notepad2 extends JPanel {
 				frame.getContentPane().add("Center", notepad);
 				/*frame.setJMenuBar(notepad.createMenubar());
 				frame.addWindowListener(new AppCloser());*/
+				frame.addWindowListener(new AppCloser());
 				frame.pack();
 				frame.setSize(500, 600);
 				frame.setVisible(true);
