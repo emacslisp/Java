63952a7762a78460b96a5d8385089ea3e308c3a9
emacslisp <java.gdb@gmail.com>
2017-07-19 23:38:25 +1000

    add test class for spring

---
+++ Spring2/src/main/java/com/diwu/spring/test/App.java
@@ -0,0 +1,11 @@
+package com.diwu.spring.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person = new Person();
+		person.speak();
+	}
+
+}
---
+++ Spring2/src/main/java/com/diwu/spring/test/Person.java
@@ -0,0 +1,7 @@
+package com.diwu.spring.test;
+
+public class Person {
+	public void speak() {
+		System.out.println("This is person class!!!");
+	}
+}
