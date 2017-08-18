0daa1c307acf5454744c3b80af818fb6380d7784
emacslisp <java.gdb@gmail.com>
2017-08-18 12:18:13 +1000

    AC leetcode 419

---
+++ JavaMain/src/com/dw/leetcode/Battleships_in_a_Board_419.java
@@ -0,0 +1,28 @@
+package com.dw.leetcode;
+
+public class Battleships_in_a_Board_419 {
+	
+    public int countBattleships(char[][] board) {
+        if(board.length == 0) return 0;
+        
+    	int m = board.length; int n= board[0].length;
+    	
+    	int counter = 0;
+    	for(int i=0;i<m;i++)
+    		for(int j=0;j<n;j++) {
+    			if(board[i][j] == '.') continue;
+    			if(i>0 && board[i-1][j] == 'X') continue;
+    			if(j>0 && board[i][j-1] == 'X') continue;
+    			counter ++;
+    		}
+    	
+    	return counter;
+    }
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
