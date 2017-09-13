25e0612dc5d369948fe02faa4965d27def0ab8fe
emacslisp <java.gdb@gmail.com>
2017-09-14 09:21:12 +1000

    add junit into eclipse classpath and create an simple junit testing

---
+++ JavaMain/src/com/dw/lib/test/jtidyTestTest.java
@@ -0,0 +1,16 @@
+package com.dw.lib.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jtidyTestTest {
+
+	@Test
+	public void test()
+	{
+		int i = 1+1;
+		assertEquals(i,2);
+	}
+
+}
