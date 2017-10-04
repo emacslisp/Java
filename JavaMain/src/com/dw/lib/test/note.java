c01744eaf45ed4037db9fdcc1ed6a2a09fb394d1
emacslisp <java.gdb@gmail.com>
2017-10-04 14:29:25 +1100

    finish mini log4j

--- JavaMain/src/com/dw/lib/test/note.java
+++ JavaMain/src/com/dw/lib/test/note.java
@@ -1,11 +1,15 @@
 package com.dw.lib.test;
 
+import java.io.File;
+
 public class note {
 
 	//@example: All lib test code here
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String dataserverDir = new File(".").getAbsolutePath();
+		System.out.println(dataserverDir);
 	}
 
 }
