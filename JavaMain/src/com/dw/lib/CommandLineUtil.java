dc97896b342ba2ddbf6dae492d577e52bb7380cf
emacslisp <java.gdb@gmail.com>
2017-11-01 11:29:02 +1100

    add command line util

---
+++ JavaMain/src/com/dw/lib/CommandLineUtil.java
@@ -0,0 +1,50 @@
+package com.dw.lib;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class CommandLineUtil {
+	
+	
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println(runCommand("g++ -gdwarf-2 -g3 /Users/ewu/dev/byacc/create/vectorTest.cpp -o /Users/ewu/dev/byacc/create/vectorTest"));
+		//System.out.println("test");
+	}
+	
+	public static String runCommand(String input) {
+		String result="";
+		
+		try {
+	        Process pr = Runtime.getRuntime().exec(input);
+	        BufferedReader read = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+	        String str = read.readLine();
+	        while (str != null){
+	            System.out.println(str);
+	            str = read.readLine();
+	            result += str;
+	            result += "\n";
+	        }
+	        
+	        InputStream error = pr.getErrorStream();
+	        InputStreamReader isrerror = new InputStreamReader(error);
+	        BufferedReader bre = new BufferedReader(isrerror);
+	        while ((str = bre.readLine()) != null) {
+	        	result += str;
+	            result += "\n";
+	        }
+	        
+	        result += "exit value is " + pr.exitValue();
+	        
+	    } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+		
+		return result;
+	}
+}
---
+++ JavaMain/src/com/dw/lib/test/CommandLineUtilTest.java
@@ -0,0 +1,13 @@
+package com.dw.lib.test;
+
+import com.dw.lib.CommandLineUtil;
+
+public class CommandLineUtilTest {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println(CommandLineUtil.runCommand("ls /etc"));
+	}
+
+}
