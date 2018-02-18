f2df2a93cfdf10057c39ddeb50b59d16a620cf16
emacslisp <java.gdb@gmail.com>
2018-02-18 15:50:04 +1100

    add java net utils httpdownload function to file

--- JavaMain/src/com/dw/lib/NetUtils.java
+++ JavaMain/src/com/dw/lib/NetUtils.java
@@ -1,7 +1,14 @@
 package com.dw.lib;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class NetUtils {
 	public static void ping(String host) {
@@ -21,6 +28,42 @@ public class NetUtils {
 			System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void HttpDownload(String urlPath, String path) {
+	    int len = 1024;
+	    //String path = "http://dl1.video.varzesh3.com/video/clip93/12/video/havashi/top5_save_derby_dortmond.mp4";
+	    URL url;
+		try {
+			url = new URL(urlPath);
+		
+		    URLConnection connectUrl = url.openConnection();
+		    //connectUrl.setRequestProperty("Range", "Bytes=0-1024");
+		    //System.out.println(len = connectUrl.getContentLength());
+		    System.out.println(connectUrl.getContentType());
+		    connectUrl.connect();
+		    File file = new File(path);
+		    
+		    OutputStream outputStream = null;
+		    outputStream = new FileOutputStream(file);
+		    InputStream inputStream = connectUrl.getInputStream();
+		    int read = 0;
+			byte[] bytes = new byte[len];
+			int off = 0;
+		    while ((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+				
+			}
+		    
+		    outputStream.flush();
+		    outputStream.close();
+	    
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e){
 			e.printStackTrace();
 		}
 	}
--- JavaMain/src/com/dw/lib/test/NetUtilsTest.java
+++ JavaMain/src/com/dw/lib/test/NetUtilsTest.java
@@ -6,7 +6,16 @@ public class NetUtilsTest {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//NetUtils.ping("www.google.com");
+		HttpDownloadToFileTest();
+	}
+	
+	public static void ping() {
 		NetUtils.ping("www.google.com");
+	}
+	
+	public static void HttpDownloadToFileTest() {
+		NetUtils.HttpDownload("http://www.google.com.au", "/Users/di.wu/test/1.html");
 	}
 	
 }
