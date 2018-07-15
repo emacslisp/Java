0642568c075bb36c95dfdc5f9a72bfd495887f8f
emacslisp <java.gdb@gmail.com>
2018-07-15 15:08:41 +1000

    add java ip address helper

---
+++ JavaMain/src/com/dw/lib/IpAddressHelper.java
@@ -0,0 +1,18 @@
+package com.dw.lib;
+
+/*
+ * 
+ * */
+public class IpAddressHelper {
+	
+	public boolean IsPrivateIpAddress() {
+		boolean result = false;
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
