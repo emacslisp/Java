e33aa84a6741c459c7b9834ce23464cdf6e66ee1
emacslisp <java.gdb@gmail.com>
2018-07-28 14:27:16 +1000

    add jget command

---
+++ JavaMain/src/com/dw/get/Downloader.java
@@ -0,0 +1,10 @@
+package com.dw.get;
+
+public class Downloader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ JavaMain/src/com/dw/get/IDownloader.java
@@ -0,0 +1,5 @@
+package com.dw.get;
+
+public interface IDownloader {
+
+}
