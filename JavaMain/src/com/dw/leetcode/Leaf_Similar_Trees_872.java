c6fed641725139a374bf8aad435c00c14bb0eb77
emacslisp <java.gdb@gmail.com>
2018-08-04 00:00:04 +1000

    add 872 and TreeNode

---
+++ JavaMain/src/com/dw/leetcode/Leaf_Similar_Trees_872.java
@@ -0,0 +1,25 @@
+package com.dw.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leaf_Similar_Trees_872 {
+	
+	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        boolean result = false;
+        
+        List<Integer> ArrayList = new ArrayList<>();
+        
+        return result;
+    }
+	
+	public void foo(TreeNode node) {
+		if(node.left == null && node.right == null) {
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
