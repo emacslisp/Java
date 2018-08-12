0f29c95ecca818ebb0344da9e4b5ea2f2ed295c1
emacslisp <java.gdb@gmail.com>
2018-08-12 14:26:28 +1000

    finish UUID cli

--- JavaMain/src/com/dw/lib/test/UUIDCLI.java
+++ JavaMain/src/com/dw/lib/test/UUIDCLI.java
@@ -7,12 +7,13 @@ public class UUIDCLI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			if (args.length == 0)
-				System.out.println(UUID.randomUUID());
-
-			for (String s : args) {
-				System.out.println(UUID.fromString(s));
+			if (args.length > 0){
+				System.out.println("UUID didn't take any arguements");
+				return;
 			}
+			
+			System.out.println(UUID.randomUUID());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
