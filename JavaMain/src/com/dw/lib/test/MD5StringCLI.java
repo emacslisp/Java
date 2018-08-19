2efa0ff3c57f381814d916331dc470c9cfb9b01e
emacslisp <java.gdb@gmail.com>
2018-08-19 20:38:34 +1000

    add md5 command line CLI

--- JavaMain/src/com/dw/lib/test/MD5StringCLI.java
+++ JavaMain/src/com/dw/lib/test/MD5StringCLI.java
@@ -1,20 +1,21 @@
 package com.dw.lib.test;
 
 import com.dw.lib.MD5Generator;
-import com.dw.lib.NetUtils;
 
-public class MD5FileCLI {
+public class MD5StringCLI {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		if(args.length != 1) {
-			System.out.println("md5file <file path>");
+			System.out.println("md5str <file path>");
 			return;
 		}
 		
 		try {
 			MD5Generator m = new MD5Generator();
-			if(args.length == 1) 
-				System.out.println(m.generateFileValue(args[0]));
+			if(args.length == 1) {
+				System.out.println(m.generateValue(args[0]));
+			}
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
--- JavaMain/src/com/dw/lib/test/NetUtilsCLI.java
+++ JavaMain/src/com/dw/lib/test/NetUtilsCLI.java
@@ -6,7 +6,7 @@ public class NetUtilsCLI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		if(args.length == 0) {
-			System.out.println("get <ULR>");
+			System.out.println("get <URL>");
 			System.out.println("get <URL> <output file> ");
 			return;
 		}
