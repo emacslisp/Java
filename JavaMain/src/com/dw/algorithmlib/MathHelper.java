52cc8e9d6d7f9565e0858505c940cb4ff4c4d97f
emacslisp <java.gdb@gmail.com>
2018-09-05 21:30:48 +1000

    add isSquare check

---
+++ JavaMain/src/com/dw/algorithmlib/MathHelper.java
@@ -0,0 +1,16 @@
+package com.dw.algorithmlib;
+
+public class MathHelper {
+	
+    public boolean isSquare(int n) {
+    	int t = (int)Math.sqrt(n);
+    	
+    	return t*t == n;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
