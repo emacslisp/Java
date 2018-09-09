90a34d036532f6a8f1dc1ecd2cfb2987e287b755
emacslisp <java.gdb@gmail.com>
2018-09-09 22:56:18 +1000

    add leetcode 17 (not done yet)

---
+++ JavaMain/src/com/dw/leetcode/Letter_Combinations_of_a_Phone_Number_17.java
@@ -0,0 +1,25 @@
+package com.dw.leetcode;
+
+import java.util.*;
+
+public class Letter_Combinations_of_a_Phone_Number_17 {
+	static final char[][] number = { { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' }, { 'j', 'k', 'l' },
+			{ 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' }, { 'w', 'x', 'y', 'z' }
+	};
+	
+    public List<String> letterCombinations(String digits) {
+    	List<String> result = new ArrayList<String>();
+    	if(digits.length() == 0){
+            return new ArrayList<>(); 
+        }
+    	
+    	
+    	return result;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
