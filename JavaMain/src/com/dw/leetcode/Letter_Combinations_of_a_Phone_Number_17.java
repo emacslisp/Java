23436ab5c73087bef0d911120cf4b838e6a840f6
emacslisp <java.gdb@gmail.com>
2018-09-11 20:21:59 +1000

    finish 17

--- JavaMain/src/com/dw/leetcode/Letter_Combinations_of_a_Phone_Number_17.java
+++ JavaMain/src/com/dw/leetcode/Letter_Combinations_of_a_Phone_Number_17.java
@@ -13,13 +13,33 @@ public class Letter_Combinations_of_a_Phone_Number_17 {
             return new ArrayList<>(); 
         }
     	
-    	
+    	foo(digits, 0, 0, digits.length(), new ArrayList<Character>(), result);
     	return result;
+    }
+    
+    public void foo(String digits, int index, int total, int len, List<Character> temp, List<String> result) {
+    	if(total == len) {
+    		StringBuilder stringBuilder = new StringBuilder();
+    		for(Character c: temp) {
+    			stringBuilder.append(c);
+    		}
+    		result.add(stringBuilder.toString());
+    		return;
+    	}
+    	
+    	int numberIndex = Integer.parseInt(Character.toString(digits.charAt(index)));
+    	
+    	for(char c: number[numberIndex - 2]) {
+    		temp.add(c);
+    		foo(digits, index+1, total+1, len, temp, result);
+    		temp.remove(temp.size() - 1);
+    	}
     }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Letter_Combinations_of_a_Phone_Number_17 s = new Letter_Combinations_of_a_Phone_Number_17();
+		System.out.println(s.letterCombinations("234"));
 	}
 
 }
