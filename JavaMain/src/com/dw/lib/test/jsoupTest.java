8461b9a625972dde366280295e0412626929fb6f
emacslisp <java.gdb@gmail.com>
2018-09-16 15:06:29 +1000

    add dict.org into jdict command

--- JavaMain/src/com/dw/lib/test/jsoupTest.java
+++ JavaMain/src/com/dw/lib/test/jsoupTest.java
@@ -9,14 +9,27 @@ public class jsoupTest {
 	
 	public static void printByClass(Document doc, String className) {
 		Elements e = doc.getElementsByClass(className);
+		if(e == null) return;
 		String output = e.toString();
 		if(output.equals("")) return;
 		
 		System.out.println(jtidyTest.html2text(output));
 	}
 	
+	public static void printByTag(Document doc, String tagName) {
+		Elements e = doc.getElementsByTag(tagName);
+		if(e == null) return;
+		String output = e.toString();
+		if(output.equals("")) return;
+		
+		for(Element x: e) {
+			System.out.println(jtidyTest.html2text(x.toString()));
+		}
+	}
+	
 	public static void printUsage() {
 		System.out.println("jdict [word list]");
+		System.out.println("jdict -e [word list]");
 	}
 	
 	public static void main(String[] args) {
@@ -25,6 +38,13 @@ public class jsoupTest {
 			
 			if(args.length <= 0) {
 				printUsage();
+			}
+			
+			boolean e2e = false;
+			int index = 0;
+			if(args[0] == "-e") {
+				e2e = true;
+				index = 1;
 			}
 			
 			//String word = args[0];
@@ -42,12 +62,17 @@ public class jsoupTest {
 					"layout nwd"
 					};
 
-			for (int i = 0; i < args.length; i++) {
+			
+			for (int i = index; i < args.length; i++) {
 				System.out.println(String.format("%s: ", args[i]));
 
 				Document doc = Jsoup.connect(String.format("http://dict.cn/%s", args[i])).get();
 				for (String s : className)
 					printByClass(doc, s);
+				if(e2e) {
+					Document docE2E = Jsoup.connect(String.format("http://www.dict.org/bin/Dict?Form=Dict2&Database=*&Query=%s", args[i])).get();
+					printByTag(docE2E,"pre");
+				}
 			}
 			
 		} catch (IOException e) {
