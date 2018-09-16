92cefeb03222124c145281a5450acad712302730
emacslisp <java.gdb@gmail.com>
2018-09-16 11:21:05 +1000

    finish first version of youdao

---
+++ JavaMain/src/com/dw/lib/test/YouDaoCLI.java
@@ -0,0 +1,75 @@
+package com.dw.lib.test;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class YouDaoCLI {
+	public static void printByClass(Document doc, String className) {
+		Elements e = doc.getElementsByClass(className);
+		String output = e.toString();
+		if(output.equals("")) return;
+		
+		System.out.println(jtidyTest.html2text(output));
+	}
+	
+	public static void printById(Document doc, String id) {
+		Element e = doc.getElementById(id);
+		String output = e.toString();
+		if(output.equals("")) return;
+		
+		System.out.println(jtidyTest.html2text(output));
+	}
+	
+	public static void printUsage() {
+		System.out.println("youdao [word list]");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			
+			if(args.length <= 0) {
+				printUsage();
+			}
+			
+			//String word = args[0];
+			
+			String[] className = {
+					
+					};
+			
+			String[] IdList = {
+					"phrsListTab",
+					"webTransToggle",
+					"tPETrans-all-trans",
+					"collinsResult",
+					"synonyms",
+					"relWordTab",
+					"bilingual",
+					"originalSound",
+					"authority"
+					};
+
+			for (int i = 0; i < args.length; i++) {
+				System.out.println(String.format("%s: ", args[i]));
+
+				Document doc = Jsoup.connect(String.format("http://www.youdao.com/w/eng/%s", args[i])).get();
+				for (String s : className)
+					printByClass(doc, s);
+				
+				for (String s : IdList)
+					printById(doc, s);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
