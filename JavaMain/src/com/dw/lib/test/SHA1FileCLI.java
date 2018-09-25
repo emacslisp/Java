07c02f91bddcd66b8201131f0e3ba083abf223e7
emacslisp <java.gdb@gmail.com>
2018-09-25 10:08:59 +1000

    add sha1 CLI

---
+++ JavaMain/src/com/dw/lib/test/SHA1FileCLI.java
@@ -0,0 +1,26 @@
+package com.dw.lib.test;
+
+import com.dw.lib.CryptoGenerator;
+
+public class SHA1FileCLI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		if(args.length != 1) {
+			System.out.println("sha1file <file path>");
+			return;
+		}
+		
+		try {
+			CryptoGenerator m = new CryptoGenerator(CryptoGenerator.CryptoType.SHA_1);
+			if(args.length == 1) 
+				System.out.println(m.generateFileValue(args[0]));
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
---
+++ JavaMain/src/com/dw/lib/test/SHA1StringCLI.java
@@ -0,0 +1,24 @@
+package com.dw.lib.test;
+
+import com.dw.lib.CryptoGenerator;
+
+public class SHA1StringCLI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length != 1) {
+			System.out.println("sha1file <file path>");
+			return;
+		}
+		
+		try {
+			CryptoGenerator m = new CryptoGenerator(CryptoGenerator.CryptoType.SHA_1);
+			if(args.length == 1) 
+				System.out.println(m.generateValue(args[0]));
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
