260d03c38721f03c732b17b7ce92e6da640f1bf9
emacslisp <java.gdb@gmail.com>
2018-10-14 22:58:24 +1100

    add hibernate, log4jHelper, MongodbHelper

---
+++ JavaMain/src/com/dw/lib/Log4jHelper.java
@@ -0,0 +1,5 @@
+package com.dw.lib;
+
+public class Log4jHelper {
+
+}
---
+++ JavaMain/src/com/dw/lib/MongodbHelper.java
@@ -0,0 +1,10 @@
+package com.dw.lib;
+
+public class MongodbHelper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
