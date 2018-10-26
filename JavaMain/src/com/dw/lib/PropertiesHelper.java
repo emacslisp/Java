17beddea1b797e6616ae0c8d89392bbf24c64a7b
emacslisp <java.gdb@gmail.com>
2018-10-26 22:53:44 +1100

    finish Properties Helper

--- JavaMain/src/com/dw/lib/PropertiesHelper.java
+++ JavaMain/src/com/dw/lib/PropertiesHelper.java
@@ -1,15 +1,39 @@
 package com.dw.lib;
 
 import java.util.Properties;
+import java.io.*;
 
 public class PropertiesHelper {
-	
+
+	private Properties _prop;
+
 	public PropertiesHelper() {
-		
+		_prop = new Properties();;
+	}
+
+	public void readProperties(String fileName) {
+		try {
+			InputStream in = PropertiesHelper.class.getResourceAsStream("/" + fileName);
+			BufferedReader bf = new BufferedReader(new InputStreamReader(in));
+			_prop.load(bf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * get value by key
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public String getProperty(String key) {
+		return _prop.getProperty(key);
 	}
 	
-	public void loadPropertiesInResources() {
-		/*Properties properties = new Properties();
-        properties.load();*/
+	public static void main(String[] args) {
+		PropertiesHelper helper = new PropertiesHelper();
+		helper.readProperties("hibernate.properties");
+		System.out.println(helper.getProperty("hibernate.connection.url"));
 	}
 }
