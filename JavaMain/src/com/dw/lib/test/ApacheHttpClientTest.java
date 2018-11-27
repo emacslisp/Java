f34b99e847ee15deb58c8b7a4e55a29cf98a8230
emacslisp <java.gdb@gmail.com>
2018-11-27 15:11:25 +1100

    fix http client

--- JavaMain/src/com/dw/lib/test/ApacheHttpClientTest.java
+++ JavaMain/src/com/dw/lib/test/ApacheHttpClientTest.java
@@ -17,14 +17,6 @@ public class ApacheHttpClientTest {
 		HttpClient client = HttpClientBuilder.create().build();
 		HttpGet request = new HttpGet("https://jsonplaceholder.typicode.com/todos/1");
 		
-		java.util.logging.Logger.getLogger("org.apache.http.wire").setLevel(java.util.logging.Level.FINEST);
-		java.util.logging.Logger.getLogger("org.apache.http.headers").setLevel(java.util.logging.Level.FINEST);
-		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
-		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
-		System.setProperty("org.apache.commons.logging.simplelog.log.httpclient.wire", "ERROR");
-		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "ERROR");
-		System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.headers", "ERROR");
-		
 		HttpResponse response;
 		try {
 			response = client.execute(request);
