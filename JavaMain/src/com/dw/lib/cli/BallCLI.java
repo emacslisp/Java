36fc6e2a7fb8c5a99ac3078af253967e884ebd2f
emacslisp <java.gdb@gmail.com>
2019-01-30 06:18:55 +1100

    #ball - optimize Ball generation algorithm

--- JavaMain/src/com/dw/lib/cli/BallCLI.java
+++ JavaMain/src/com/dw/lib/cli/BallCLI.java
@@ -9,7 +9,6 @@ public class BallCLI {
 		String uuid = UUID.randomUUID().toString();
 		uuid = uuid.replace("-", "");
 
-		int counter = 0;
 		int total = 0;
 		for (int i = 0; i < uuid.length(); i++) {
 			char c = uuid.charAt(i);
@@ -23,15 +22,14 @@ public class BallCLI {
 			}
 
 			int base = 1;
-			for (int j = 0; j < counter; j++) {
+			for (int j = 0; j < i; j++) {
 				base *= 16;
 				base %= baseNum;
 			}
-			counter++;
 
 			total += num * base;
 		}
-		return total;
+		return total % baseNum;
 	}
 
 	public static void generate() {
@@ -39,8 +37,7 @@ public class BallCLI {
 
 		for (int k = 0; k < 8; k++) {
 			if (k < 7) {
-				int total = BallRandom(35);
-				int result = total % 35;
+				int result = BallRandom(35);
 
 				if (!hash.contains(result)) {
 					System.out.print((result == 0? 35: result) + " ");
@@ -50,8 +47,7 @@ public class BallCLI {
 					continue;
 				}
 			} else {
-				int total = BallRandom(20);
-				int result = total % 20;
+				int result = BallRandom(20);
 				System.out.print(result == 0? 20: result);
 			}
 		}
