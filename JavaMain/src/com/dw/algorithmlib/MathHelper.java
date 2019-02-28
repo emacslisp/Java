d5894cbb27c9b7c7bbb53f9f8ff9037453e6ca60
emacslisp <java.gdb@gmail.com>
2019-02-28 23:04:46 +1100

    add java library for algorithm

--- JavaMain/src/com/dw/algorithmlib/MathHelper.java
+++ JavaMain/src/com/dw/algorithmlib/MathHelper.java
@@ -7,6 +7,22 @@ public class MathHelper {
     	
     	return t*t == n;
     }
+    
+    // @todo: add two large number using String
+    public String add(String num1, String num2) {
+    	String result = "";
+    	
+    	
+    	
+    	return result;
+    }
+    
+    // @todo: multiply two large number using String
+    public String Multiple(String num1, String num2) {
+    	String result = "";
+    	
+    	return result;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ JavaMain/src/com/dw/algorithmlib/SortHelper.java
@@ -0,0 +1,13 @@
+package com.dw.algorithmlib;
+
+public class SortHelper {
+    //@todo: sort pair object here
+	public void SortPair(Pair[] pair) {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
