6bc330ea980a19eabf1cfab56fa02736f643fa91
emacslisp <java.gdb@gmail.com>
2019-03-21 22:57:14 +1100

    #BinaryTree - add mirror, delete tree and inOrder Print

--- JavaMain/src/com/dw/algorithmlib/BinaryTree.java
+++ JavaMain/src/com/dw/algorithmlib/BinaryTree.java
@@ -43,6 +43,72 @@ public class BinaryTree {
         else
             return(size(node.left) + 1 + size(node.right)); 
     } 
+    
+    void mirror() 
+    { 
+        root = mirror(root); 
+    } 
+  
+    TreeNode mirror(TreeNode node) 
+    { 
+        if (node == null) 
+            return node; 
+  
+        /* do the subtrees */
+        TreeNode left = mirror(node.left); 
+        TreeNode right = mirror(node.right); 
+  
+        /* swap the left and right pointers */
+        node.left = right; 
+        node.right = left; 
+  
+        return node; 
+    } 
+    
+    void inOrder() 
+    { 
+        inOrder(root); 
+    } 
+  
+    /* Helper function to test mirror(). Given a binary 
+       search tree, print out its data elements in 
+       increasing sorted order.*/
+    void inOrder(TreeNode node) 
+    { 
+        if (node == null) 
+            return; 
+  
+        inOrder(node.left); 
+        System.out.print(node.data + " "); 
+  
+        inOrder(node.right); 
+    } 
+    
+    void deleteTree(TreeNode node) 
+    { 
+        // In Java automatic garbage collection 
+        // happens, so we can simply make root 
+        // null to delete the tree 
+        root = null; 
+    } 
+      
+    /* Wrapper function that deletes the tree and  
+       sets root node as null  */
+    void deleteTreeRef(TreeNode nodeRef) 
+    { 
+    	if(nodeRef == null) return;
+    	
+    	
+    	deleteTreeRef(nodeRef.left);
+
+    	deleteTreeRef(nodeRef.right);
+    	
+    	
+   	 System.out.println("Deleting node:" + nodeRef.data);
+   	 deleteTree(nodeRef); 
+         
+        nodeRef=null; 
+    } 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -59,6 +125,20 @@ public class BinaryTree {
         
         System.out.println("The size of binary tree is : "
                 + tree.size()); 
+        
+        System.out.println("Inorder traversal of input tree is :"); 
+        tree.inOrder(); 
+        System.out.println(""); 
+        
+        /* convert tree to its mirror */
+        tree.mirror(); 
+  
+        /* print inorder traversal of the minor tree */
+        System.out.println("Inorder traversal of binary tree is : "); 
+        tree.inOrder(); 
+        
+        tree.deleteTreeRef(tree.root); 
+        System.out.println("Tree deleted"); 
 	}
 
 }
