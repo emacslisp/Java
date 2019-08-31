832b33cdec59830971f30ea3ddfdbeba4b4f18ba
emacslisp <java.gdb@gmail.com>
2019-09-01 01:38:28 +1000

    finish adding simple spring debug

---
+++ SpringFrameworkDebug/src/com/lab/SpringDebug.java
@@ -0,0 +1,14 @@
+package com.lab;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		Message obj = (Message) context.getBean("Message");
+		obj.getMessage();
+	}
+}
