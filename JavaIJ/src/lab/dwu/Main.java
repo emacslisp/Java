d9b233765509e60fa75d6e5a26d3853899561716
emacslisp <java.gdb@gmail.com>
2019-11-28 08:40:49 +1100

    add java intellij project

---
+++ JavaIJ/src/lab/dwu/Main.java
@@ -0,0 +1,11 @@
+package lab.dwu;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(new Date());
+    }
+}
