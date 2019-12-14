94d58c61cab839183541e30c482478b6ce1c1fc9
emacslisp <java.gdb@gmail.com>
2019-12-14 14:31:56 +1100

    # jsp bean validation

--- JSPServlet/src/com/lab/User.java
+++ JSPServlet/src/com/lab/User.java
@@ -4,9 +4,16 @@ public class User {
 	private String email;
 	private String password;
 	
-	public User(String string, String string2) {
+	private String message;
+	
+	public User(String username, String password) {
 		// TODO Auto-generated constructor stub
+		this.email = username;
+		this.password = password;
 	}
+	
+	
+	
 	public String getEmail() {
 		return email;
 	}
@@ -20,8 +27,35 @@ public class User {
 		this.password = password;
 	}
 	
-    public static void main(String[] args) {
-        System.out.println("Java Bean in Console");
-    }
-
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public boolean validate() {
+		
+		if(this.email == null) {
+			this.message = "No Email Address Set";
+			return false;
+		}
+		
+		if(!email.matches("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b")) {
+			message = "invalid email";
+			return false;
+		}
+		
+		if(this.password == null) {
+			this.message = "No password Set";
+			return false;
+		}
+		
+		if(this.password.length() < 8) {
+			message = "password must be at lease 8 characters.";
+			return false;
+		}
+		else if(this.password.matches("\\w*\\s+\\w*")) {
+			message = "password cannot contain space.";
+			return false;
+		}
+		return true;
+	}
 }
--- JSPServlet/src/com/lab/cli/UserAuthenticationWithSalt.java
+++ JSPServlet/src/com/lab/cli/UserAuthenticationWithSalt.java
@@ -4,6 +4,13 @@ import com.lab.User;
 
 public class UserAuthenticationWithSalt {
     public static void main(String[] args) {
-        User user = new User("java.gdb@gmail.com", "123456");
+        User user = new User("java.gdb@gmail.com", "123456789");
+       
+        if(user.validate()) {
+        	System.out.println("Bean validates OK.");
+        }
+        else {
+        	System.out.println(user.getMessage());
+        }
     }
 }
