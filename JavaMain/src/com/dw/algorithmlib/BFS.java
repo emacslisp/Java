0678fcbf970f25aa923a089623a2e956496a0083
emacslisp <java.gdb@gmail.com>
2020-02-25 09:35:26 +1100

    finish one of BFS algorithm

--- JavaMain/src/com/dw/algorithmlib/BFS.java
+++ JavaMain/src/com/dw/algorithmlib/BFS.java
@@ -1,10 +1,68 @@
 package com.dw.algorithmlib;
 
-public class BFS {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+import java.util.HashMap;
+
+public class BFS {
+	public void bfsVisit(int n, int[][] connections) {
+		
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        
+        for (int[] connection : connections) {
+            connect(graph, connection[0], connection[1]);
+            connect(graph, connection[1], connection[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        for (int v = 0; v < n; v++) {
+            if (!visited[v]) {
+                bfs(v, graph, visited);
+            }
+        }
+
+	}
+	
+	private void bfs(int src, HashMap<Integer, List<Integer>> graph, boolean[] visited) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.offer(src);
+		
+		visited[src] = true;
+		System.out.println("Point " + src + " is visited");
+		while(!queue.isEmpty()) {
+			int node = queue.poll();
+			
+			List<Integer> n = graph.get(node);
+			System.out.println("Point " + node + " is dequeued");
+			if(n==null) continue;
+			for(int m : n) {
+				if(!visited[m]) {
+					visited[m] = true;
+					queue.offer(m);
+					System.out.println("Point " + m + " is visited");
+				}
+			}
+		}
+	}
+
+	public void connect(HashMap<Integer, List<Integer>> graph, Integer v1, Integer v2) {
+		// TODO Auto-generated method stub
+		if (graph.get(v1) == null) graph.put(v1, new LinkedList<>());
+		graph.get(v1).add(v2);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[][] connections = {{17,51},{33,83},{53,62},{25,34},{35,90},{29,41},{14,53},{40,84},{41,64},{13,68},{44,85},{57,58},{50,74},{20,69},{15,62},{25,88},{4,56},{37,39},{30,62},{69,79},{33,85},{24,83},{35,77},{2,73},{6,28},{46,98},{11,82},{29,72},{67,71},{12,49},{42,56},{56,65},{40,70},{24,64},{29,51},{20,27},{45,88},{58,92},{60,99},{33,46},{19,69},{33,89},{54,82},{16,50},{35,73},{19,45},{19,72},{1,79},{27,80},{22,41},{52,61},{50,85},{27,45},{4,84},{11,96},{0,99},{29,94},{9,19},{66,99},{20,39},{16,85},{12,27},{16,67},{61,80},{67,83},{16,17},{24,27},{16,25},{41,79},{51,95},{46,47},{27,51},{31,44},{0,69},{61,63},{33,95},{17,88},{70,87},{40,42},{21,42},{67,77},{33,65},{3,25},{39,83},{34,40},{15,79},{30,90},{58,95},{45,56},{37,48},{24,91},{31,93},{83,90},{17,86},{61,65},{15,48},{34,56},{12,26},{39,98},{1,48},{21,76},{72,96},{30,69},{46,80},{6,29},{29,81},{22,77},{85,90},{79,83},{6,26},{33,57},{3,65},{63,84},{77,94},{26,90},{64,77},{0,3},{27,97},{66,89},{18,77},{27,43}};
+		
+		int n = 100;
+		
+		BFS bfs = new BFS();
+		bfs.bfsVisit(n, connections);
 	}
 
 }
