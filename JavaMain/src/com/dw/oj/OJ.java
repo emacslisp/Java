f8be6dcbd9eea7dd99dba4c33d1d5dc93d82a860
emacslisp <java.gdb@gmail.com>
2020-03-17 00:10:21 +1100

    add oj main file

--- JavaMain/src/com/dw/oj/OJ.java
+++ JavaMain/src/com/dw/oj/OJ.java
@@ -2,6 +2,12 @@ package com.dw.oj;
 
 public class OJ {
 
+	// 1. fetch question in html including image
+	// 2. get submit file from local
+	// 3. combine submitted file with src from database and make it becomes a true
+	// 4. build and run java code to generate result csv file
+	// 5. comparing result csv file with answer csv file
+	// 6. security - may be ignore it at the first.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
