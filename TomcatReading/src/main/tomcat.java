b622ae902c9070885c5e1f6184325e3c013a1120
emacslisp <java.gdb@gmail.com>
2020-03-27 07:38:52 +1100

    add tomcat

--- TomcatReading/src/main/tomcat.java
+++ TomcatReading/src/main/tomcat.java
@@ -5,13 +5,23 @@ import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.util.Diagnostics;
 
 public class tomcat {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Diagnostics();
+		
+		getProperty();
+	}
+	
+	public static void getProperty() {
+		System.out.println(System.getProperty("user.dir"));
+	}
+	
+	public static void Diagnostics() {
 		Log log = LogFactory.getLog(tomcat.class);
 		log.info("this is a info message from tomcat");
 		log.debug("this is a debug message from tomcat");
 		
 		System.out.println(Diagnostics.getVMInfo());
 	}
+
+	
 }
