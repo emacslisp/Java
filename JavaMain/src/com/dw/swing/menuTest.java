982dc71d3e1145094d48cebb6008800ef3b9beb1
emacslisp <java.gdb@gmail.com>
2020-05-27 02:03:24 +1000

    fix menuTest

--- JavaMain/src/com/dw/swing/menuTest.java
+++ JavaMain/src/com/dw/swing/menuTest.java
@@ -1,12 +1,12 @@
 package com.dw.swing;
 
-import com.dw.swing.lib.NMJFrame;
+import com.dw.swing.lib.DFrame;
 
 public class menuTest {
 //@todo: Java Project for Swing as file explore
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		NMJFrame frame = new NMJFrame("a Test of encapulated frame object");
+		DFrame frame = new DFrame("a Test of encapulated frame object");
 		frame.setVisible(true);
 	}
 }
