b768f66f8c0108adf9bba09103031c6206d86f97
emacslisp <java.gdb@gmail.com>
2020-06-09 00:40:28 +1000

    start Java Home Ftp Project

---
+++ JavaMain/src/com/dw/ftp/UserAuth.java
@@ -0,0 +1,11 @@
+package com.dw.ftp;
+
+public class UserAuth {
+
+	// connect to mysql to check username and password
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaMain/src/com/dw/ftp/cmd/UserCommandHandler.java
@@ -0,0 +1,12 @@
+package com.dw.ftp.cmd;
+
+import com.dw.ftp.FtpCommandHandler;
+
+public class UserCommandHandler extends FtpCommandHandler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
