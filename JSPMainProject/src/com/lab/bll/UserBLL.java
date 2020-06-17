858ae20f21d8e77607cf6ad1cd1adbf083201c39
emacslisp <java.gdb@gmail.com>
2020-06-17 16:38:24 +1000

    finish JNDI using tomcat

--- JSPMainProject/src/com/lab/bll/UserBLL.java
+++ JSPMainProject/src/com/lab/bll/UserBLL.java
@@ -1,13 +1,74 @@
 package com.lab.bll;
 
-public class UserBLL {
+import java.sql.*;
 
+import javax.naming.*;
+import javax.sql.DataSource;
+
+public class UserBLL {
+	Connection conn = null;
 	public UserBLL() {
-		
+		conn = initDataSource();
 	}
 	
 	public boolean validUserNamePassword(String username, String password) {
-		return true;
+			
+		if(conn != null) {
+			Statement stmt = null;
+			try {
+				stmt = conn.createStatement();
+				String sql = "select * from User where username = '" + username + "' and password = '" + password + "'";
+				ResultSet rs = stmt.executeQuery(sql);
+				System.out.println("=========== execute JNDI query successfully");
+				int count = 0;
+				while (rs.next()) {
+				    ++count;
+				}
+				stmt.close();
+				
+				if (count > 0) {
+					return true;
+				}
+				
+				return false;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if(stmt!= null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+		
+		return false;
+	}
+	
+	public Connection initDataSource() {
+		Context initContext;
+		try {
+			initContext = new InitialContext();
+		
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource)envContext.lookup("jdbc/Test");
+			Connection conn = ds.getConnection();
+			
+			return conn;
+		} catch (NamingException  e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
