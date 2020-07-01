9e289bb460b22aaaf9e597c2e6f1a21856358acf
emacslisp <java.gdb@gmail.com>
2020-07-01 16:19:08 +1000

    add MessageBox for warning and error

--- JavaMain/src/com/dw/swing/lib/MessageBox.java
+++ JavaMain/src/com/dw/swing/lib/MessageBox.java
@@ -12,4 +12,14 @@ public class MessageBox {
     {
         JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
     }
+	
+	public static void warning(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.WARNING_MESSAGE);
+    }
+	
+	public static void error(String infoMessage, String titleBar)
+    {
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.ERROR_MESSAGE);
+    }
 }
