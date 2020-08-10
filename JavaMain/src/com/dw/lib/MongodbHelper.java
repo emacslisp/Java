7b17e927d16457efee22268adb60850e583760c6
emacslisp <java.gdb@gmail.com>
2020-08-10 23:21:26 +1000

    add mongodb and postgresql helper for java

--- JavaMain/src/com/dw/lib/MongodbHelper.java
+++ JavaMain/src/com/dw/lib/MongodbHelper.java
@@ -1,8 +1,12 @@
 package com.dw.lib;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
+import com.mongodb.client.model.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
 public class MongodbHelper {
 	
@@ -21,10 +25,28 @@ public class MongodbHelper {
 	
 		return conn;
 	}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String host = "127.0.0.1";
+		int port = 27017;
+		MongoClient conn = new MongoClient(host, port);
+		MongoDatabase db = conn.getDatabase("vocus-one");
+		MongoCollection<Document> coll = db.getCollection("billingaccounts");
+		// BasicDBObject obj = new BasicDBObject();
+		// obj.put("billingSystemId", 54146);
+		// String query = "db.getCollection('billingaccounts').find({'billingSystemId': '54146'})";
+		Bson input = Document.parse("{'billingSystemId': '54146'}");
+		
+		MongoCursor<Document> result = coll.find(input).iterator();
+		JsonOp jsonop = new JsonOp();
+		while(result.hasNext()) {
+			Document doc = result.next();
+			System.out.println(jsonop.JsonFormater(doc.toJson()));
+		}
+		conn.close();
 	}
 
 }
