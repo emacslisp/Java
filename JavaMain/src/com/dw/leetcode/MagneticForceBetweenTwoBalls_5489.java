83f3c44de97cd70baf47c3194b7a073d2e2d9a2c
emacslisp <java.gdb@gmail.com>
2020-08-17 00:17:14 +1000

    make src compiled

--- JavaMain/src/com/dw/leetcode/MagneticForceBetweenTwoBalls_5489.java
+++ JavaMain/src/com/dw/leetcode/MagneticForceBetweenTwoBalls_5489.java
@@ -6,7 +6,8 @@ public class MagneticForceBetweenTwoBalls_5489 {
 	public int maxDistance(int[] position, int m) {
 		Arrays.sort(position);
 		
-		
+		int result = 0;
+		return result;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
