3394a7c6052c685a028075036738597c1172d164
emacslisp <java.gdb@gmail.com>
2020-08-19 09:56:28 +1000

    add LinkedMultiValueMap and its Entry

--- SpringFrameworkReading/src/org/spring/main/SpringMain.java
+++ SpringFrameworkReading/src/org/spring/main/SpringMain.java
@@ -1,10 +1,34 @@
 package org.spring.main;
 
+import java.util.List;
+
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
 public class SpringMain {
+	
+	public void MultiValueMapEntry() {
+		MultiValueMap<String, String> multiValueMaps = new LinkedMultiValueMap<String, String>();
+		multiValueMaps.add("Tom", "Book");
+		multiValueMaps.add("Tom", "Pen");
+		
+		multiValueMaps.add("ABC", "Company");
+		multiValueMaps.add("ABC", "WebSite");
+		
+		for(String key: multiValueMaps.keySet()) {
+			List<String> value = multiValueMaps.get(key);
+			System.out.print(key + "\t");
+			for(String s: value) {
+				System.out.print(s + "\t");
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SpringMain s = new SpringMain();
+		s.MultiValueMapEntry();
 	}
 
 }
