7e384f40e54bde05078a5a0f691833cc1bfcb075
emacslisp <java.gdb@gmail.com>
2020-10-03 11:39:52 +1000

    add junit test for TDD(test drive development)

---
+++ JSPMainProject/src/com/lab/test/TestEntry.java
@@ -0,0 +1,15 @@
+package com.lab.test;
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestEntry {
+
+	@Test
+	public void loginTest() {
+		int i = 1;
+		Assert.assertEquals(i, 1);
+	}
+
+}
