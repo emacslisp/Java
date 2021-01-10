978aca76ea86bb2b418be04df9c182149d0bd3f6
emacslisp <java.gdb@gmail.com>
2021-01-11 09:44:56 +1100

    make jcat print out multiple files

--- JavaMain/src/com/dw/lib/cli/CatCLI.java
+++ JavaMain/src/com/dw/lib/cli/CatCLI.java
@@ -8,17 +8,21 @@ public class CatCLI {
 
 	public static void main(String[] args) {
 		try {
-			if (args.length != 1){
+			if (args.length < 1){
 				System.out.println("jcat to view a text file");
 				System.out.println("jcat <text file>");
 				return;
 			}
 			
 			FileUtils utils = new FileUtils();
-			List<String> result = utils.fileToList(args[0]);
-			for(String s : result) {
-				System.out.println(s);
+			for(int i=0;i<args.length;i++) {
+				System.out.println(args[i] + ":");
+				List<String> result = utils.fileToList(args[i]);
+				for(String s : result) {
+					System.out.println(s);
+				}
 			}
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
