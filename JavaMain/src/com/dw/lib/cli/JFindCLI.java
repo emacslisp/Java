ee01f2c721bbe51c627133b3e97bdda6cdc9183a
emacslisp <java.gdb@gmail.com>
2021-01-22 16:06:11 +1100

    add two new CLI with empty java file

---
+++ JavaMain/src/com/dw/lib/cli/JFindCLI.java
@@ -0,0 +1,11 @@
+package com.dw.lib.cli;
+
+// find file name under folder using LCS algorithm
+public class JFindCLI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaMain/src/com/dw/lib/cli/OJCLI.java
@@ -0,0 +1,10 @@
+package com.dw.lib.cli;
+
+public class OJCLI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
