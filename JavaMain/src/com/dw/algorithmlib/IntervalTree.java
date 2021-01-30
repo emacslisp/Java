a9b3cce74ce165541ad569c5912946662d31e7ca
emacslisp <java.gdb@gmail.com>
2021-01-31 01:21:19 +1100

    add java interval tree

---
+++ JavaMain/src/com/dw/algorithmlib/IntervalTree.java
@@ -0,0 +1,10 @@
+package com.dw.algorithmlib;
+
+public class IntervalTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- JavaMain/src/com/dw/lib/test/Print.java
+++ JavaMain/src/com/dw/lib/test/Print.java
@@ -4,4 +4,12 @@ public class Print {
 	public static void print(String s) {
 		System.out.println(s);
 	}
+	
+	public static void main(String[] args) {
+		String p = "/asdf/asdf/i";
+		String[] inputs = p.split("/");
+		for(String x: inputs) {
+			System.out.println(x);
+		}
+	}
 }
