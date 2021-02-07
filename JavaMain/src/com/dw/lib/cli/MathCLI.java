05504a684279476fe9d9eaa42a31d6e85458d495
emacslisp <java.gdb@gmail.com>
2021-02-07 17:59:00 +1100

    add Math CLI to calculate large number

---
+++ JavaMain/src/com/dw/lib/cli/MathCLI.java
@@ -0,0 +1,11 @@
+package com.dw.lib.cli;
+
+public class MathCLI {
+	
+	// (+ 1 (- 5 3) (* 4 5) (pow 2 31))
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
