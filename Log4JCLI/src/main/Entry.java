a0707d98d52648a700a9870efe975fa893853467
emacslisp <java.gdb@gmail.com>
2021-05-07 14:12:51 +1000

    finish log4j source code

--- Log4JCLI/src/main/Entry.java
+++ Log4JCLI/src/main/Entry.java
@@ -1,10 +1,15 @@
 package main;
 
-public class Entry {
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
+public class Entry {
+	static Logger logger = Logger.getLogger(Entry.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DOMConfigurator.configure(Entry.class.getResource("log4j.xml"));
+		logger.debug("This is messaging");
 	}
 
 }
