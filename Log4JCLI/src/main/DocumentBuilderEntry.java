60291f855e0bda835c35c7c3d50aff5678e27a15
emacslisp <java.gdb@gmail.com>
2021-05-16 12:07:20 +1000

    finish example of DocumentBuilderEntry

--- Log4JCLI/src/main/DocumentBuilderEntry.java
+++ Log4JCLI/src/main/DocumentBuilderEntry.java
@@ -8,12 +8,26 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class DocumentBuilderEntry {
 
-	
+	public static void loop(Node node) {
+	    // do something with the current node instead of System.out
+	    System.out.println(node.getNodeName());
+
+	    NodeList nodeList = node.getChildNodes();
+	    for (int i = 0; i < nodeList.getLength(); i++) {
+	        Node currentNode = nodeList.item(i);
+	        if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+	            //calls this method for all the children which is Element
+	        	loop(currentNode);
+	        }
+	    }
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -29,7 +43,7 @@ public class DocumentBuilderEntry {
 			  InputSource src = new InputSource(stream);
 			  src.setSystemId(url.toString());
 			  Document doc = parser.parse(src);
-			  
+			  loop(doc.getDocumentElement());
 			} catch (SAXException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
