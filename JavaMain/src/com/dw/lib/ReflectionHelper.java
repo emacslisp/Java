ae995a361584f99058e1e39c0c0a6bbb1c1181a2
emacslisp <java.gdb@gmail.com>
2021-06-19 13:07:57 +1000

    add ReflectionHelper

---
+++ JavaMain/src/com/dw/lib/ReflectionHelper.java
@@ -0,0 +1,18 @@
+package com.dw.lib;
+
+public class ReflectionHelper {
+
+	public ReflectionHelper() {
+		
+	}
+	
+	public <T> String getName(Class<T> c) {
+		return c.getName();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
