ec653c661e9a283ed05acec0e419bcd7ca4d0267
emacslisp <java.gdb@gmail.com>
2021-06-20 13:43:14 +1000

    update security settings and new project SprintBootCLI

--- SpringBootRestfulAPI/resources/schema.sql
+++ SpringBootRestfulAPI/resources/schema.sql
@@ -11,4 +11,7 @@ CREATE TABLE IF NOT EXISTS User (
   INDEX(UserName)
 ) engine=InnoDB;
 
-insert into User(UserName, Password, Roles) values('Admin', '123456', 'ADMIN,USER'); 
+insert into User(UserName, Password, Roles) values('Admin', '123456', 'ROLE_ADMIN,ROLE_USER'); 
+insert into User(id, user_name, password, roles, active) values(1, 'Admin', '123456', 'ROLE_ADMIN, ROLE_USER', true);
+
+update User set roles = 'ROLE_ADMIN, ROLE_USER' where id = 1
