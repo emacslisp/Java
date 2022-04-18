1db6a323f98b1dcf416640a56e3fba0947b422d2
emacslisp <java.gdb@gmail.com>
2022-04-18 19:40:40 +1000

    add '-l' to show line number for cat

--- JavaMain/src/com/dw/lib/cli/CatCLI.java
+++ JavaMain/src/com/dw/lib/cli/CatCLI.java
@@ -10,15 +10,29 @@ public class CatCLI {
 		try {
 			if (args.length < 1){
 				System.out.println("jcat to view a text file");
-				System.out.println("jcat <text file>");
+				System.out.println("jcat -l <text file>");
 				return;
 			}
 			
 			FileUtils utils = new FileUtils();
+			boolean state = false;
+			
 			for(int i=0;i<args.length;i++) {
+				boolean showLine = args[i].equals("-l");
+				if (showLine)  {
+					state = true;
+					continue;
+				}
+				
 				System.out.println(args[i] + ":");
 				List<String> result = utils.fileToList(args[i]);
+				int lineNumber = 1;
+				
 				for(String s : result) {
+					if (state) {
+						System.out.print(lineNumber + ":\t");
+						lineNumber++;
+					}
 					System.out.println(s);
 				}
 			}
